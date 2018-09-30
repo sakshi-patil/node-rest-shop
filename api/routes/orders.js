6e930f3ddd17113213de8fd0d4d6ad96873730b8
sakshi-patil <sakshipatil2805@gmail.com>
2018-09-30 18:02:40 +0530

    error handling

---
+++ api/routes/orders.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handlng GET request to /orders'
+    });
+});
+
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling POST request to /orders'
+    });
+});
+
+router.get('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+    if (id === 'special'){
+        res.status(200).json({
+            message: 'You discovered the special ID',
+        });
+    }
+    else{
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+
+router.patch('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling patch request to /orders'
+    });
+});
+
+router.delete('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling delete request to /orders'
+    });
+});
+
+module.exports = router;
+
+
--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+
+app.use(morgan('dev'));
+
+app.use('/products',productRoutes);
+app.use('/orders',orderRoutes);
+
 
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It Works!'
+    const error = new Error('not found');
+    error.status(404);
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        },
+        msg:'inside error handler'
     });
 });
 
