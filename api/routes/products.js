86681f052a2d7560525688185e10fc82129ae53d
sakshi-patil <sakshipatil2805@gmail.com>
2018-10-02 17:48:23 +0530

    6 modified product routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -74,15 +74,45 @@ router.get('/:productId', (req, res, next) => {
 
 
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling patch request to /products'
-    });
+
+
+    const id=req.params.productId;
+    const updateOps={};
+    for (const ops of req.body){
+        updateOps[ops.propName]=ops.value;
+    }
+
+    Product.update({_id:id }, { $set: updateOps})
+        .exec()
+        .then(result=>{
+            console.log(result);
+            res.status(200).json(result);
+
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+        });
+
 });
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling delete request to /products'
-    });
+
+    const id = req.params.productId;
+
+    Product.remove({_id: id})
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 module.exports = router;
