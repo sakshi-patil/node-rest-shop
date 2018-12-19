27d88b55b136a8b71b96ae2166ef3ca512fabd35
sakshi-patil <sakshipatil2805@gmail.com>
2018-12-19 16:22:22 +0530

    Signup and login

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # node-rest-shop
+'''
+
 RESTful node project to build a shop api
+ 
+''''
--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -1,13 +1,21 @@
+
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
+module.exports = function (req, res, next) {
+
   try {
+
+    const token = req.headers.authorization.split(" ")[1];
+    console.log("token=" + token);
     const decoded = jwt.verify(token, process.env.JWT_KEY);
     req.userData = decoded;
     next();
+
   } catch (error) {
     return res.status(401).json({
-      message: 'Auth failed'
+      message: "Token Auth Failed"
     });
   }
+
+
 };
